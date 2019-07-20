b0f4febd470130f414cf742e68793ff593a67c5f
sheng10086@gmail.com <sheng10086@gmail.com>
2019-07-20 14:28:41 -0400

    able to load regular data to mysql database but not able to dealwith cells with double quotes

--- src/main/java/com/ezops/FileUploadServlet.java
+++ src/main/java/com/ezops/FileUploadServlet.java
@@ -1,25 +1,114 @@
 package com.ezops;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.logging.Level;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 import org.springframework.stereotype.Controller;
+
+import com.opencsv.CSVReader;
 
 @Controller
 @WebServlet(name = "FileUploadServlet", urlPatterns = {"/upload"})
 @MultipartConfig
 public class FileUploadServlet extends HttpServlet {
-	     
+	//Mysql for local database
+	private String dbURL = "jdbc:mysql://localhost:3306/ezops";
+    private String dbUser = "root";
+    private String dbPass = "12345";   
+	
 	protected void doPost(HttpServletRequest request,
 	        HttpServletResponse response)
 	        throws ServletException, IOException {
-	    System.out.println("Able to map the HttpSerletRequest!!!");
+		response.setContentType("text/html;charset=UTF-8");
+	    System.out.println("Working Directory = " +System.getProperty("user.dir"));
+	    
+	    
+	    final Part filePart = request.getPart("file");
+	    InputStream filecontent = null;
+	    
+	    filecontent = filePart.getInputStream();
+        try {
+			ReadCSV(filecontent);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	    
 	}
-
+	
+	private String getFileName(final Part part) {
+	    final String partHeader = part.getHeader("content-disposition");
+	    for (String content : part.getHeader("content-disposition").split(";")) {
+	        if (content.trim().startsWith("filename")) {
+	            return content.substring(
+	                    content.indexOf('=') + 1).trim().replace("\"", "");
+	        }
+	    }
+	    return null;
+	}
+	
+	private void ReadCSV(InputStream filecontent) throws IOException, SQLException{	
+		CSVReader reader=null;
+		Connection conn=null;
+		try{
+       	 	reader = new CSVReader(new InputStreamReader(filecontent), ',','\'', 1);
+       	 	DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());   
+       	 	conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
+       	 	String insertQuery = "Insert into titanic (passenger_id,survied,class_of_travel,"
+       	 			+ "passenger_firstname,passenger_lastname,sex, age, number_of_sibspouse_aboard, number_of_parent_aboard,"
+       	 			+ "ticket,Fare, Cabin,Embarked) "
+       	 			+ "values (?,?,?,?,?,?,?,?,?,?,?,?,?)";
+       	 	PreparedStatement pstmt = conn.prepareStatement(insertQuery);
+            String[] rowData = null;
+            int i = 0;
+            while((rowData = reader.readNext()) != null){   
+            	System.out.println(rowData);
+                 for (String data : rowData)
+                {       
+               	 	System.out.println("row data is  "+ data);
+               	 	System.out.println(i%13+1);
+               	 	//System.out.println("i is "+i);
+               	 	
+               	 	
+                        pstmt.setString((i % 13+1), data);
+                        if (++i % 13 == 0)
+                                pstmt.addBatch();// add batch
+                        
+                        if (i % 13 == 0)// insert when the batch size is 10
+                       	 {
+                       	  System.out.println("you have that batch size");
+                       	 int[] rows2 = pstmt.executeBatch();
+                       	 System.out.println(Arrays.toString(rows2));
+                      
+                }
+            }
+     }
+            System.out.println("Data Successfully Uploaded");
+    }catch (Exception e){
+          e.printStackTrace();
+    	}finally{
+    		if(conn!=null){
+    			conn.close();
+    		}
+    		if(reader!=null){
+    			reader.close();
+    		}
+    	}
+	}
 }
+
